ccf51d5b9d34fa737a75acfd6a67946fdb955191
Nicholas Dan <nicholaseiti@gmail.com>
2023-07-04 11:51:45 -0300

    Botão para voltar para o menu

--- ui-manager.js
+++ ui-manager.js
@@ -1,8 +1,14 @@
 export class UIManager{
     static #scoreEl = document.getElementById('scoreEl');
     static #resultadoEl = document.getElementById('Result');
+    static #returnEl = document.getElementById('return');
     static #gameOverSound = new Audio('./res/sounds/gameOver.wav');
     static #gameWinSound = new Audio('./res/sounds/gameWin.wav');
+
+    constructor(){
+        UIManager.#returnEl.style.visibility = 'hidden';
+    }
+
     updateScore(newScore){
         UIManager.#scoreEl.innerText = newScore;
     }
@@ -15,11 +21,13 @@ export class UIManager{
         UIManager.#resultadoEl.innerText = "Você PERDEU, mais sorte na próxima vez =D !";
         UIManager.#resultadoEl.style.color = 'red';
         UIManager.#gameOverSound.play();
+        UIManager.#returnEl.style.visibility = 'visible';
     }
 
     win(){
         UIManager.#resultadoEl.innerText = "Você Ganhou, PARABÉNS !!";
         UIManager.#resultadoEl.style.color = 'green';
         UIManager.#gameWinSound.play();
+        UIManager.#returnEl.style.visibility = 'visible';
     }
 }
